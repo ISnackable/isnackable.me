bf755cb07f46522acb0dc096cf69855ff0fcccb8
Tommy <52971804+ISnackable@users.noreply.github.com>
2021-10-18 22:00:07 +0800

    Add header and footer template

--- gatsby-web/src/components/footer.js
+++ gatsby-web/src/components/footer.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "gatsby";
+import * as styles from "./footer.module.css";
 
 const Footer = () => {
-  return <footer className="">Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <button>Contact</button>
+      <span></span>
+      {/* dynamic content here */}
+      <Link to="https://github.com/ISnackable">GitHub</Link>
+    </footer>
+  );
 };
 
 export default Footer;
