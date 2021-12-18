5263fe5bb29f7e6e8dc61c3603c5e69869019c41
Tommy <52971804+ISnackable@users.noreply.github.com>
2021-12-18 15:37:31 +0800

    Add modal component and fix postcss

--- gatsby-web/postcss.config.js
+++ gatsby-web/postcss.config.js
@@ -1,13 +1,6 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV || "development"}`,
-});
-
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(isProd ? { cssnano: {} } : {}),
   },
 };
