ca85579b29507094b19e9209dbbe9438cc1731a9
Tommy <52971804+ISnackable@users.noreply.github.com>
2022-01-25 09:22:06 +0800

    chore: upgrade packages

--- sanity-studio/schemas/documents/blockContent.js
+++ sanity-studio/schemas/documents/blockContent.js
@@ -1,3 +1,15 @@
+import "ace-builds/src-noconflict/mode-assembly_x86";
+import "ace-builds/src-noconflict/mode-sh";
+import "ace-builds/src-noconflict/mode-c_cpp";
+import "ace-builds/src-noconflict/mode-diff";
+import "ace-builds/src-noconflict/mode-dockerfile";
+import "ace-builds/src-noconflict/mode-graphqlschema";
+import "ace-builds/src-noconflict/mode-lua";
+import "ace-builds/src-noconflict/mode-nginx";
+import "ace-builds/src-noconflict/mode-powershell";
+import "ace-builds/src-noconflict/mode-rust";
+import "ace-builds/src-noconflict/mode-swift";
+
 /**
  * This is the schema definition for the rich text fields used for
  * for this blog studio. When you import it in schemas.js it can be
@@ -95,16 +107,17 @@ export default {
       options: {
         lanugage: "javascript",
         languageAlternatives: [
+          { title: "Nasm", value: "nasm", mode: "assembly_x86" },
           { title: "Batch file", value: "batchfile" },
-          { title: "Bash", value: "bash" },
-          { title: "C", value: "c" },
-          { title: "C++", value: "cpp" },
+          { title: "Bash", value: "bash", mode: "sh" },
+          { title: "C", value: "c", mode: "c_cpp" },
+          { title: "C++", value: "cpp", mode: "c_cpp" },
           { title: "C#", value: "csharp" },
           { title: "CSS", value: "css" },
-          { title: "Diff", value: "diff" },
-          { title: "Docker", value: "docker" },
+          { title: "Diff", value: "diff", mode: "diff" }, // use custom renderer on frontend
+          { title: "Docker", value: "docker", mode: "dockerfile" },
           { title: "Go", value: "golang" },
-          { title: "GraphQL", value: "graphql" },
+          { title: "GraphQL", value: "graphql", mode: "graphqlschema" },
           { title: "GROQ", value: "groq" },
           { title: "HTTP", value: "http" },
           { title: "HTML", value: "html" },
@@ -112,20 +125,21 @@ export default {
           { title: "JavaScript", value: "javascript" },
           { title: "JSON", value: "json" },
           { title: "JSX", value: "jsx" },
-          { title: "Lua", value: "lua" },
+          { title: "Lua", value: "lua", mode: "lua" },
           { title: "Markdown", value: "markdown" },
           { title: "MySQL", value: "mysql" },
-          { title: "Nginx", value: "nginx" },
+          { title: "Nginx", value: "nginx", mode: "nginx" },
           { title: "PHP", value: "php" },
           { title: "Plain text", value: "text" },
-          { title: "Powershell", value: "powershell" },
+          { title: "Powershell", value: "powershell", mode: "powershell" },
           { title: "Python", value: "python" },
-          { title: "Regex", value: "regex" },
+          { title: "Regex", value: "regex", mode: "text" },
           { title: "Ruby", value: "ruby" },
-          { title: "Rust", value: "rust" },
+          { title: "Rust", value: "rust", mode: "rust" },
           { title: "SASS", value: "sass" },
           { title: "SCSS", value: "scss" },
           { title: "sh", value: "sh" },
+          { title: "Swift", value: "swift", mode: "swift" },
           { title: "TSX", value: "tsx" },
           { title: "TypeScript", value: "typescript" },
           { title: "XML", value: "xml" },
