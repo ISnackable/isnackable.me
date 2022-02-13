b0aaa17c01b9a2f9a0ecc690fae61ae758d113fa
Tommy <52971804+ISnackable@users.noreply.github.com>
2022-02-13 15:50:18 +0800

    Remove Gatsby Files

--- gatsby-web/client-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  sanity: {
-    projectId:
-      process.env.GATSBY_SANITY_STUDIO_API_PROJECT_ID ||
-      "<#< sanity.projectId >#>",
-    dataset:
-      process.env.GATSBY_SANITY_STUDIO_API_DATASET || "<#< sanity.dataset >#>",
-  },
-};
