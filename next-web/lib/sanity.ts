107c98498cb1a44aaecfd21829f6f960ec92b765
Tommy <52971804+ISnackable@users.noreply.github.com>
2022-02-16 18:17:34 +0800

    chore: setting up nextjs routes

--- next-web/lib/sanity.ts
+++ next-web/lib/sanity.ts
@@ -1,10 +1,9 @@
 import {
   createImageUrlBuilder,
-  createPortableTextComponent,
   createPreviewSubscriptionHook,
   createCurrentUserHook,
 } from "next-sanity";
-import { sanityConfig } from "./config";
+import { sanityConfig } from "./sanity.server";
 
 /**
  * Set up a helper function for generating Image URLs with only the asset reference data in your documents.
@@ -16,22 +15,6 @@ export const urlFor = (source) =>
 // Set up the live preview subscription hook
 export const usePreviewSubscription =
   createPreviewSubscriptionHook(sanityConfig);
-
-// Set up Portable Text serialization
-export const PortableText = createPortableTextComponent({
-  ...sanityConfig,
-  // Serializers passed to @sanity/block-content-to-react
-  // (https://github.com/sanity-io/block-content-to-react)
-  serializers: {
-    types: {
-      code: (props) => (
-        <pre data-language={props.node.language}>
-          <code>{props.node.code}</code>
-        </pre>
-      ),
-    },
-  },
-});
 
 // Helper function for using the current logged in user account
 export const useCurrentUser = createCurrentUserHook(sanityConfig);
