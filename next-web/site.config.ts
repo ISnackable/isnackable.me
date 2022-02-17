cd26465d9676352b2b3962f03439e4b58f9942ed
Tommy <52971804+ISnackable@users.noreply.github.com>
2022-02-17 13:42:33 +0800

    Remove Tailwindcss

---
+++ next-web/site.config.ts
@@ -0,0 +1,8 @@
+export default {
+  title: "ISnackable, Cybersecurity",
+  titleTemplate: "%s – ISnackable, Cybersecurity",
+  description: "A personal site",
+  siteUrl: "https://www.isnackable.me",
+  image: "icons/icon-512x512.png", // Path to your image you placed in the 'static' folder
+  socialUsername: "@ISnackable",
+};
