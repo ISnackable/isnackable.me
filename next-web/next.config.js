2ab0dfdc17399927aaf06fba7a420a7eab93010b
Tommy <52971804+ISnackable@users.noreply.github.com>
2022-02-19 16:09:19 +0800

    feat: add PWA

--- next-web/next.config.js
+++ next-web/next.config.js
@@ -1,10 +1,44 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+const headers = async () => {
+  return [
+    {
+      source: "/(.*)",
+      headers: [
+        {
+          key: "X-Content-Type-Options",
+          value: "nosniff"
+        },
+        {
+          key: "X-Frame-Options",
+          value: "SAMEORIGIN"
+        },
+        {
+          key: "X-XSS-Protection",
+          value: "1; mode=block"
+        }
+      ]
+    }
+  ];
+};
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   i18n: {
     locales: ["en"],
-    defaultLocale: "en",
+    defaultLocale: "en"
   },
-};
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development"
+  },
+  headers
+});
 
 module.exports = nextConfig;
--- next-web/src/components/seo.tsx
+++ next-web/src/components/seo.tsx
@@ -17,12 +17,36 @@ const SEO: NextPage<SEOProps> = ({ title, description, image, article }) => {
     title: title || siteConfig.title,
     description: description || siteConfig.description,
     image: `${siteConfig.siteUrl}/${image || siteConfig.image}`,
-    url: `${siteConfig.siteUrl}${asPath}`,
+    url: `${siteConfig.siteUrl}${asPath}`
   };
 
   return (
     <Head>
       <title>{`${seo.title} – ${siteConfig.title}`}</title>
+      <meta charSet="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
+      />
+      <meta
+        name="theme-color"
+        content="#1a1b1e"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link rel="manifest" href="/manifest.webmanifest" />
+      <link rel="apple-touch-icon" href="/icon.png"></link>
+      <link
+        href="/icons/favicon-16x16.png"
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+      />
+      <link
+        href="/icons/favicon-32x32.png"
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+      />
       <meta name="description" content={seo.description} />
       {seo.image && <meta property="og:image" content={seo.image} />}
       {seo.title && <meta property="og:title" content={seo.title} />}
