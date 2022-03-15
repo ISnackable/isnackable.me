1852fef6f570811f78b6961d3bf9b1842394422e
Tommy <52971804+ISnackable@users.noreply.github.com>
2022-03-15 17:51:54 +0800

    chore: optimize import, giscus colorscheme

--- next-web/src/components/BlogPost/BlogPost.tsx
+++ next-web/src/components/BlogPost/BlogPost.tsx
@@ -1,6 +1,13 @@
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
-import { Avatar, Center, Container, Text, Title } from "@mantine/core";
+import {
+  Avatar,
+  Center,
+  Container,
+  Text,
+  Title,
+  useMantineColorScheme
+} from "@mantine/core";
 import SanityNextImage from "@components/SanityNextImage";
 import { toDateString } from "@lib/helpers";
 import { urlFor } from "@lib/sanity";
@@ -30,71 +37,72 @@ interface Props {
 const BlogPost: NextPage<Props> = (props) => {
   const { post } = props;
 
-  return (
-    <>
-      <Container size="xl" my="6rem">
-        <section>
-          <Title order={1}>{post.title}</Title>
-          <Text size="xl">{post.description}</Text>
-          <div style={{ margin: "2rem 0px" }}>
-            <Avatar
-              src={urlFor(post.author.mainImage).auto("format").url()}
-              alt={post.author.name}
-              radius="xl"
-              size="lg"
-              mr="1rem"
-              sx={() => ({ float: "left" })}
-            />
+  const { colorScheme } = useMantineColorScheme();
+  const dark = colorScheme === "dark";
 
-            <div>
-              <Text weight={600} size="md">
-                {post.author.name}
-              </Text>
-              <Text size="md">{toDateString(post.publishedAt)}</Text>
-            </div>
+  return (
+    <Container size="xl" my="6rem">
+      <section>
+        <Title order={1}>{post.title}</Title>
+        <Text size="xl">{post.description}</Text>
+        <div style={{ margin: "2rem 0px" }}>
+          <Avatar
+            src={urlFor(post.author.mainImage).auto("format").url()}
+            alt={post.author.name}
+            radius="xl"
+            size="lg"
+            mr="1rem"
+            sx={() => ({ float: "left" })}
+          />
+
+          <div>
+            <Text weight={600} size="md">
+              {post.author.name}
+            </Text>
+            <Text size="md">{toDateString(post.publishedAt)}</Text>
           </div>
-          <Center>
-            <SanityNextImage
-              image={post.mainImage}
-              height={200}
-              className="rounded-lg object-cover"
-              alt={post.mainImage?.alt ?? `${post.title} main image`}
-            />
-          </Center>
-        </section>
+        </div>
+        <Center>
+          <SanityNextImage
+            image={post.mainImage}
+            height={200}
+            className="rounded-lg object-cover"
+            alt={post.mainImage?.alt ?? `${post.title} main image`}
+          />
+        </Center>
+      </section>
+      <section
+        style={{
+          marginTop: 32,
+          maxWidth: "55rem",
+          marginLeft: "auto",
+          marginRight: "auto",
+          wordWrap: "break-word"
+        }}
+      >
+        {post.body.length > 0 && (
+          <SanityPortableText value={post.body} onMissingComponent={false} />
+        )}
+      </section>
+      {giscusRepo && giscusRepoId && giscusCategoryId && (
         <section
           style={{
-            marginTop: 32,
-            maxWidth: "55rem",
-            marginLeft: "auto",
-            marginRight: "auto",
-            wordWrap: "break-word"
+            marginTop: 32
           }}
         >
-          {post.body.length > 0 && (
-            <SanityPortableText value={post.body} onMissingComponent={false} />
-          )}
+          <Giscus
+            repo={giscusRepo}
+            repoId={giscusRepoId}
+            category={giscusCategory}
+            categoryId={giscusCategoryId}
+            mapping={giscusMapping}
+            reactionsEnabled={giscusReactionsEnabled}
+            emitMetadata={giscusEmitMetadata}
+            theme={dark ? "dark" : "light"}
+          />
         </section>
-        {giscusRepo && giscusRepoId && giscusCategoryId && (
-          <section
-            style={{
-              marginTop: 32
-            }}
-          >
-            <Giscus
-              repo={giscusRepo}
-              repoId={giscusRepoId}
-              category={giscusCategory}
-              categoryId={giscusCategoryId}
-              mapping={giscusMapping}
-              reactionsEnabled={giscusReactionsEnabled}
-              emitMetadata={giscusEmitMetadata}
-              theme="dark"
-            />
-          </section>
-        )}
-      </Container>
-    </>
+      )}
+    </Container>
   );
 };
 
--- next-web/src/pages/files.tsx
+++ next-web/src/pages/files.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from "next";
+import dynamic from "next/dynamic";
 import { Container } from "@mantine/core";
 import SEO from "@components/SEO";
-import Redirect from "@components/Redirect";
+
+const Redirect = dynamic(() => import("@components/Redirect"), {
+  ssr: false
+});
 
 const FilesPage: NextPage = () => {
   return (
