600e71c87208269bc83b32eef105dfc54cfbbf50
Tommy <52971804+ISnackable@users.noreply.github.com>
2023-11-28 17:13:47 +0800

    fix(web): upgrade sanity webhook and fixes revalidate

--- next-web/src/pages/api/revalidate.ts
+++ next-web/src/pages/api/revalidate.ts
@@ -6,7 +6,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 import type { NextApiRequest, NextApiResponse } from "next";
-import { isValidRequest } from "@sanity/webhook";
+import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
 import { JSend } from "../../@types/JSend";
 
 const SANITY_REVALIDATE_SECRET = process.env.SANITY_REVALIDATE_SECRET ?? "";
@@ -22,15 +22,19 @@ export default async function handler(
       .json({ status: "fail", message: "Must be a POST request" });
   }
 
-  if (!isValidRequest(req, SANITY_REVALIDATE_SECRET)) {
+  const signature = req.headers[SIGNATURE_HEADER_NAME]?.toString();
+  const body = await readBody(req); // Read the body into a string
+
+  if (
+    signature &&
+    !(await isValidSignature(body, signature, SANITY_REVALIDATE_SECRET))
+  ) {
     res.status(401).json({ status: "fail", message: "Invalid signature" });
     return;
   }
 
   try {
-    const {
-      body: { type, slug }
-    } = req;
+    const { type, slug } = JSON.parse(body);
 
     switch (type) {
       case "post":
@@ -47,4 +51,19 @@ export default async function handler(
       .status(500)
       .send({ status: "error", message: "Error revalidating" });
   }
+}
+
+// Next.js will by default parse the body, which can lead to invalid signatures
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+async function readBody(readable: NextApiRequest) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks).toString("utf8");
 }
--- sanity-studio/src/deskStructure/blog.ts
+++ sanity-studio/src/deskStructure/blog.ts
@@ -32,7 +32,7 @@ import SocialPreview from "part:social-preview/component";
 const previewURL = rootURLs[env].web;
 
 const childDocument = (documentId) =>
-  S.document()
+S.documentList()
     .documentId(documentId)
     .schemaType("post")
     .views([
